3e47dd84970d7d2cfe1fa9db0fd55680a1851600
moe-moaid <mohammadmoaid@gmail.com>
2023-08-01 17:24:33 +0500

    part of the hero section is done

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="sticky top-0 flex justify-between p-2 max-w-7xl mx-auto z-20 ">
+    <header className="sticky top-0 flex justify-between p-2 max-w-7xl mx-auto z-20">
       <Socials/>
     </header>
   );
--- user-components/circles.tsx
+++ user-components/circles.tsx
@@ -1,15 +1,29 @@
+'use client';
 import React from 'react'
+import { motion } from 'framer-motion';
 
 type Props = {}
 
 export default function Circles({}: Props) {
   return (
-    <div>
-        <div className='border border-gray-500 animate-ping rounded-full h-[200px] w-[200px]'/>
-        <div />
-        <div />
-        <div />
-        <div />
-    </div>
+    <motion.div
+    initial={{
+        opacity:0,
+    }}
+    animate={{
+        scale:[1,2,2,3,1],
+        opacity:[0.1, 0.2, 0.4, 0.8, 0.1, 1.0], 
+        borderRadius:["20%", "20%", "50%", "80%", "20%"],
+    }}
+    transition={{
+        duration: 2.5
+    }}
+    className='relative flex justify-center items-center'>
+        <div className='absolute mt-52 border border-[#333333] animate-ping rounded-full h-[200px] w-[200px]'/>
+        <div className='absolute mt-52 border border-[#333333] rounded-full h-[300px] w-[300px]'/>
+        <div className='absolute mt-52 border border-[#333333] rounded-full h-[400px] w-[400px]'/>
+        <div className='rounded-full border border-[#00C6CF] opacity-20 h-[500px] w-[500px] absolute mt-52 animate-pulse'/>
+        <div className='absolute mt-52 border border-[#333333] rounded-full h-[600px] w-[600px]'/>
+    </motion.div>
   )
 }
