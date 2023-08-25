e5faac4c2544c2e298ec99303146279929697699
moe-moaid <mohammadmoaid@gmail.com>
2023-08-25 16:24:49 +0500

    finally successfully hitting the api, but the data is not being saved

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,5 @@
 import { addAbout } from '@/actions/aboutSectionActions';
+import { About } from '@/typings';
 import React from 'react'
 
 type Props = {}
@@ -7,9 +8,9 @@ export default async function page({}: Props) {
   return (
     <div className='flex flex-col max-w-7xl justify-center items-center mx-auto'>
       <form className='flex flex-col space-y-5' action={addAbout}>
-        <input className='p-2 rounded-sm bg-gray-500/50 outline-none' name='title' type="text" placeholder='title'/>
-        <input className='p-2 rounded-sm bg-gray-500/50 outline-none' name='description' type="text" placeholder='about you'/>
-        <input id='fileInput' className='p-2 rounded-sm bg-gray-500/50' name='aboutImage' type="file"/>
+        <input className='p-2 rounded-sm bg-gray-500/50 outline-none' name='title' type="text" placeholder='title' required/>
+        <input className='p-2 rounded-sm bg-gray-500/50 outline-none' name='description' type="text" placeholder='about you' required/>
+        <input id='aboutImageInput' className='p-2 rounded-sm bg-gray-500/50' name='aboutImage' type="file" required/>
         <button className='bg-blue-500 rounded-md p-2 font-semibold text-white/80'>Enter Value</button>
       </form>
     </div>
