dd747964db067d94eb9bd0fc5a4deba04b4c9edc
moe-moaid <mohammadmoaid@gmail.com>
2023-08-28 17:31:15 +0500

    hit api using fetch traditional successfully

--- actions/aboutSectionActions.ts
+++ actions/aboutSectionActions.ts
@@ -4,31 +4,27 @@ export const addAbout = async (e:FormData) => {
   const description = e.get('description')?.toString();
   const fileInput: File | null = e.get('aboutImage') as unknown as File;
   let formData = new FormData();
-  console.log(`${title} ${description} ${fileInput}`)
   if(!title || !description || !fileInput){
     console.error("there is a missing field");
     return;
   } 
+  console.log("api hit");
+  
   formData.append("title", title);
   formData.append("description", description);
   formData.append("aboutImage", fileInput, "aboutImage");
-  const entries = formData.entries();
-  for (const [key, value] of entries) {
-    console.log(`${key}: ${value}`);
-  }
-  console.log(formData);
+  // const entries = formData.entries();
+  // for (const [key, value] of entries) {
+  //   console.log(`${key}: ${value}`);
+  // }
   const url:string = "http://172.16.150.11/my-web-api/public/api/postabout" 
-  try {
     await fetch(url, {
+      cache: "no-cache",
       method: 'POST',
       body: formData,
       headers: {
         "Content-Type": "multipart/form-data",
       }
     })
-  } catch (e) {
-    console.error(e);
-
-  }
 
 }
